1565ebd5f9abda9006848cf37f67c15998e71a51
vinaybathini9 <76684410+vinaybathini9@users.noreply.github.com>
2020-12-28 17:10:35 +0530

    Add files via upload

--- user.js
+++ user.js
@@ -1,135 +1,28 @@
-const router = require('express').Router();
-const bodyParser = require('body-parser');
-const bcrypt = require('bcryptjs');
-const { check, validationResult } = require('express-validator');
-const user = require('../models/user');
-/////middelware
-router.use(bodyParser.json());
-router.use(bodyParser.urlencoded({ extended: true }));
-
-router.all(
-    '/',
-    function(req, res) {
-        return res.json({
-            status: true,
-            message: 'user Controller working...'
-        });
-    }
-);
-
-router.post(
-    '/createNew', [
-        check('username').not().isEmpty().trim().escape(),
-        check('password').not().isEmpty().trim().escape(),
-        check('email').isEmail().normalizeEmail()
-
-    ],
-    function(req, res) {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(422).json({
-                status: false,
-                message: 'Form validation error..',
-                errors: errors.array()
-            });
-        }
+const mongoose = require('mongoose');
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        require: true
+    },
+    password: {
+        type: String,
+        require: true
+    },
+    isActive: {
+        type: Boolean,
+        require: true
+    },
+    createdON: {
+        type: Date,
+        require: Date.now()
+    },
+});
 
 
-        const hashedPassword = bcrypt.hashSync(req.body.password, 10);
-        var temp = new user({
-            username: req.body.username,
-            email: req.body.email,
-            password: hashedPassword
-        });
+mongoose.model('users', userSchema);
 
-        temp.save(function(error, result) {
-            if (error) {
-                return res.json({
-                    status: false,
-                    message: 'DB Connection fail',
-                    error: error
-                });
-            }
-            return res.json({
-                status: true,
-                message: 'success....',
-                result: result
-            });
-        });
-
-    }
-);
-router.get(
-    '/allData',
-    function(req, res) {
-        user.find(function(error, result) {
-            if (error) {
-                return res.json({
-                    status: false,
-                    message: 'Data not found',
-                    error: error
-                });
-            }
-            return res.json({
-                status: true,
-                message: 'success....',
-                result: result
-            });
-        });
-    }
-);
-
-router.put(
-    '/update/:email',
-    function(req, res) {
-        if (req.params.email) {
-            user.update({ email: req.params.email }, { $set: { username: req.body.username, password: req.body.password } }, function(error, result) {
-                if (error) {
-                    return res.json({
-                        status: false,
-                        message: 'Data is not found',
-                        error: error
-                    });
-                }
-                return res.json({
-                    status: true,
-                    message: 'success...',
-                    result: result
-                });
-            });
-        } else {
-            return res.json({
-                status: false,
-                message: 'Email is not found...'
-            });
-        }
-    }
-);
-
-router.delete(
-    '/remove/:email',
-    function(req, res) {
-        if (req.params.email) {
-            user.remove({ email: req.params.email }, function(error, result) {
-                if (error) {
-                    return res.json({
-                        status: false,
-                        message: 'Data is not found..',
-                        error: error
-                    });
-                }
-                return res.json({
-                    status: true,
-                    message: 'success....',
-                    result: result
-                });
-            });
-        } else {
-            return res.json({
-                status: false,
-                message: 'Email is not found...'
-            });
-        }
-    }
-);
-                module.exports=router;
+module.exports = mongoose.model('users');
